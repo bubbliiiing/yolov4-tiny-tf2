bc17ae0e945853f326d71679248e06a49b492b6d
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2020-11-27 09:59:15 +0800

    Update yolo.py

--- yolo.py
+++ yolo.py
@@ -15,12 +15,13 @@ from utils.utils import letterbox_image
 
 class YOLO(object):
     _defaults = {
-        "model_path"        : 'model_data/yolov4_tiny_weights_coco.h5',
+        "model_path"        : 'model_data/yolov4_tiny_weights_voc.h5',
         "anchors_path"      : 'model_data/yolo_anchors.txt',
-        "classes_path"      : 'model_data/coco_classes.txt',
+        "classes_path"      : 'model_data/voc_classes.txt',
         "score"             : 0.5,
         "iou"               : 0.3,
         "eager"             : False,
+        "max_boxes"         : 100,
         # 显存比较小可以使用416x416
         # 显存比较大可以使用608x608
         "model_image_size"  : (416, 416)
@@ -100,15 +101,15 @@ class YOLO(object):
             inputs = [*self.yolo_model.output, self.input_image_shape]
             outputs = Lambda(yolo_eval, output_shape=(1,), name='yolo_eval',
                 arguments={'anchors': self.anchors, 'num_classes': len(self.class_names), 'image_shape': self.model_image_size, 
-                'score_threshold': self.score, 'eager': True})(inputs)
+                'score_threshold': self.score, 'eager': True, 'max_boxes': self.max_boxes})(inputs)
             self.yolo_model = Model([self.yolo_model.input, self.input_image_shape], outputs)
         else:
             self.input_image_shape = K.placeholder(shape=(2, ))
             
             self.boxes, self.scores, self.classes = yolo_eval(self.yolo_model.output, self.anchors,
-                    num_classes, self.input_image_shape,
+                    num_classes, self.input_image_shape, max_boxes=self.max_boxes,
                     score_threshold=self.score, iou_threshold=self.iou)
-
+ 
     #---------------------------------------------------#
     #   检测图片
     #---------------------------------------------------#
@@ -116,7 +117,7 @@ class YOLO(object):
         start = timer()
 
         # 调整图片使其符合输入要求
-        new_image_size = self.model_image_size
+        new_image_size = (self.model_image_size[1],self.model_image_size[0])
         boxed_image = letterbox_image(image, new_image_size)
         image_data = np.array(boxed_image, dtype='float32')
         image_data /= 255.
